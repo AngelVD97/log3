104e5b5a7eecc3ee55281b346cddc3f34e188662
AngelVD97 <angeltmaes@gmail.com>
2021-10-15 19:50:45 -0500

    Actualizacion UI-ingresos

--- resources/views/informe.blade.php
+++ resources/views/informe.blade.php
@@ -4,6 +4,19 @@
 
     <style>
 
+
+/*
+ tabala css
+
+*/
+.datagrid table { border-collapse: collapse; text-align: left; width: 100%; } .datagrid {font: normal 12px/150% Arial, Helvetica, sans-serif; background: #fff; overflow: hidden; border: 1px solid #00B050; -webkit-border-radius: 5px; -moz-border-radius: 5px; border-radius: 5px; }.datagrid table td, .datagrid table th { padding: 3px 10px; }.datagrid table thead th {background:-webkit-gradient( linear, left top, left bottom, color-stop(0.05, #00B050), color-stop(1, #00B050) );background:-moz-linear-gradient( center top, #00B050 5%, #00B050 100% );filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#00B050', endColorstr='#00B050');background-color:#00B050; color:#FFFFFF; font-size: 15px; font-weight: bold; border-left: 0px solid #00B050; } .datagrid table thead th:first-child { border: none; }.datagrid table tbody td { color: #9C8E8F; font-size: 12px;border-bottom: 1px solid #00B050;font-weight: normal; }.datagrid table tbody .alt td { background: #B6FFA3; color: #00496B; }.datagrid table tbody td:first-child { border-left: none; }.datagrid table tbody tr:last-child td { border-bottom: none; }
+
+/*
+ fin tabla css
+
+*/
+
+
 h1{
     font-size: 1.5rem;
     color: #C00000;  
@@ -12,7 +25,7 @@ h1{
 
 .select_mate{
     height: 40px;
-    
+    /*display: inline-flex;*/
 }
 
 select {
@@ -30,7 +43,7 @@ select {
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
     border-radius: 0px;
     margin-bottom: 1em;
-    display: inline-flex;
+    display: flex;/*inline-flex;*/
     margin: 1rem;
 }
 
@@ -100,20 +113,23 @@ select {
 
     <h1>Informe</h1>
 
-    <div class="select_mate" data-mate-select="active" >
-<select name="" onchange="" onclick="return false;" id="">
-<option value=""  >Hoy</option>
-<option value="1">Ayer</option>
-<option value="2" >Esta Semana</option>
-<option value="3">Este Mes</option>
-  </select>
-<p class="selecionado_opcion"  onclick="open_select(this)" ></p><span onclick="open_select(this)" class="icon_select_mate" ><svg fill="#000000" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
 
-</svg></span>
-<div class="cont_list_select_mate">
-  <ul class="cont_select_int">  </ul> 
-</div>
-  </div>
+
+
+    <div class="select_mate" data-mate-select="active" >
+        <select name="" onchange="" onclick="return false;" id="">
+            <option value=""  >Hoy</option>
+            <option value="1">Ayer</option>
+            <option value="2" >Esta Semana</option>
+            <option value="3">Este Mes</option>
+        </select>
+        <p class="selecionado_opcion"  onclick="open_select(this)" ></p><span onclick="open_select(this)" class="icon_select_mate" ><svg fill="#000000" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+        </svg></span>
+        <div class="cont_list_select_mate">
+            <ul class="cont_select_int">  </ul> 
+        </div>
+    </div>
+
 
 
     
@@ -124,12 +140,12 @@ select {
                 <h2>Ingresos</h2>
             </div>
             <div class="id">
-                <i class="fas fa-dollar-sign"></i>
+                <i class="fas fa-dollar-sign"> MXN</i>
                 <img src="img/icon.svg" alt="">
             </div>
         </div>
         <div class="card_section">
-            <p>$2459</p>
+            <p>MXN2459</p>
         </div>
        
     </div>
@@ -141,12 +157,12 @@ select {
                 <h2>Ganancias</h2>
             </div>
             <div class="id">
-                <i class="fas fa-dollar-sign"></i>
+                <i class="fas fa-dollar-sign"> MXN</i>
                 <img src="img/icon.svg" alt="">
             </div>
         </div>
         <div class="card_section">
-            <p>$1000</p>
+            <p>$1000  MXN</p>
         </div>
        
     </div>
@@ -158,7 +174,7 @@ select {
                 <h2>Devoluciones</h2>
             </div>
             <div class="id">
-                <i class="fas fa-dollar-sign"></i>
+                <i class="fas fa-dollar-sign"> MXN</i>
                 <img src="img/icon.svg" alt="">
             </div>
         </div>
@@ -175,7 +191,7 @@ select {
                 <h2>Promedio Cliente</h2>
             </div>
             <div class="id">
-                <i class="fas fa-dollar-sign"></i>
+                <i class="fas fa-dollar-sign"> MXN</i>
                 <img src="img/icon.svg" alt="">
             </div>
         </div>
@@ -192,7 +208,7 @@ select {
                 <h2>Targeta Credito</h2>
             </div>
             <div class="id">
-                <i class="fas fa-dollar-sign"></i>
+                <i class="fas fa-dollar-sign"> MXN</i>
                 <img src="img/icon.svg" alt="">
             </div>
         </div>
@@ -209,7 +225,7 @@ select {
                 <h2>Targeta Debito</h2>
             </div>
             <div class="id">
-                <i class="fas fa-dollar-sign"></i>
+                <i class="fas fa-dollar-sign"> MXN</i>
                 <img src="img/icon.svg" alt="">
             </div>
         </div>
@@ -226,7 +242,7 @@ select {
                 <h2>Paypal</h2>
             </div>
             <div class="id">
-                <i class="fas fa-dollar-sign"></i>
+                <i class="fas fa-dollar-sign"> MXN</i>
                 <img src="img/icon.svg" alt="">
             </div>
         </div>
@@ -243,7 +259,7 @@ select {
                 <h2>Otro</h2>
             </div>
             <div class="id">
-                <i class="fas fa-dollar-sign"></i>
+                <i class="fas fa-dollar-sign"> MXN</i>
                 <img src="img/icon.svg" alt="">
             </div>
         </div>
@@ -253,7 +269,58 @@ select {
        
     </div>
 
-    
+<div class="datagrid">
+    <table>
+        <thead>
+            <tr>
+                <th>header</th>
+                <th>header</th>
+                <th>header</th>
+                <th>header</th>
+                <th>header</th>
+                <th>header</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>data</td>
+                <td>data</td>
+                <td>data</td>
+                <td>data</td>
+            </tr>
+            <tr class="alt">
+                <td>data</td>
+                <td>data</td>
+                <td>data</td>
+                <td>data</td>
+            </tr>
+            <tr>
+                <td>data</td>
+                <td>data</td>
+                <td>data</td>
+                <td>data</td>
+            </tr>
+            <tr class="alt">
+                <td>data</td>
+                <td>data</td>
+                <td>data</td>
+                <td>data</td>
+            </tr>
+            <tr>
+                <td>data</td>
+                <td>data</td>
+                <td>data</td>
+                <td>data</td>
+            </tr>
+            <tr class="alt">
+                <td>data</td>
+                <td>data</td>
+                <td>data</td>
+                <td>data</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
 
 
 @endsection
